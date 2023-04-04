f483ba1cb1195850f10d02c3e4c66830ad1cd606
lymo <87603845+LymoProjects@users.noreply.github.com>
2023-04-04 17:38:13 +0800

    refactor

---
+++ include/handler.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <string>
+
+#include <llapi/EventAPI.h>
+
+#include <llapi/mc/Player.hpp>
+#include <llapi/mc/Level.hpp>
+
+#include "joinMessage.hpp"
+
+namespace lymoProjects__ {
+    inline
+    auto handleCmdSet(Player * plr, std::string const & msg) -> void {
+        std::size_t spacePos {msg.find_first_of(" ")};
+
+        if (spacePos != msg.npos) {
+            joinMessage::ref().set(plr->getXuid(), msg.substr(spacePos + 1));
+
+            plr->sendText("入服通知设置成功!");
+        } else {
+            plr->sendText("格式错误! 应该为 -jm:set[空格][自定义文本]");
+        }
+    }
+
+    inline
+    auto handleCmdGet(Player * plr) -> void {
+        std::string plrMsg(joinMessage::ref().get(plr->getXuid()));
+
+        plr->sendText(
+            plrMsg.empty() ?
+            "你没有设置过任何文本!" : plrMsg
+        );
+    }
+
+    inline
+    auto handleCmdErase(Player * plr) -> void {
+        joinMessage::ref().erase(plr->getXuid());
+
+        plr->sendText("清除成功!");
+    }
+
+    inline
+    auto handlePlayerChatEvent(Event::PlayerChatEvent const & e) -> bool {
+        if (e.mMessage.starts_with("-jm:set")) {
+            handleCmdSet(e.mPlayer, e.mMessage);
+        } else if (e.mMessage.starts_with("-jm:get")) {
+            handleCmdGet(e.mPlayer);
+        } else if (e.mMessage.starts_with("-jm:erase")) {
+            handleCmdErase(e.mPlayer);
+        } else {
+            return true;
+        }
+
+        return false;
+    }
+
+    inline
+    auto handlePlayerJoinEvent(Event::PlayerJoinEvent const & e) -> bool {
+        std::string plrMsg(lymoProjects__::joinMessage::ref().get(e.mPlayer->getXuid()));
+
+        if (!plrMsg.empty()) {
+            Level::broadcastText(plrMsg, TextType::RAW);
+        }
+
+        return true;
+    }
+}
--- src/plugin.cpp
+++ src/plugin.cpp
@@ -3,58 +3,15 @@
  * @brief The main file of the plugin
  */
 
-#include <string>
-#include <string_view>
-
-#include <llapi/EventAPI.h>
-
-#include <llapi/mc/Player.hpp>
-#include <llapi/mc/Level.hpp>
-
 #include "version.h"
 
-#include "joinMessage.hpp"
+#include "handler.hpp"
 
 /**
  * @brief The entrypoint of the plugin. DO NOT remove or rename this function.
  *
  */
 void PluginInit() {
-    Event::PlayerJoinEvent::subscribe([](Event::PlayerJoinEvent const & e) {
-        std::string plrMsg(lymoProjects__::joinMessage::ref().get(e.mPlayer->getXuid()));
-
-        if (!plrMsg.empty()) {
-            Level::broadcastText(plrMsg, TextType::RAW);
-        }
-
-        return true;
-    });
-
-    Event::PlayerChatEvent::subscribe([](Event::PlayerChatEvent const & e){
-        if (e.mMessage.starts_with("-jm:set")) {
-            std::size_t spacePos {e.mMessage.find_first_of(" ")};
-
-            if (spacePos != e.mMessage.npos) {
-                lymoProjects__::joinMessage::ref().set(e.mPlayer->getXuid(), e.mMessage.substr(spacePos + 1));
-
-                e.mPlayer->sendText("入服通知设置成功!");
-            } else {
-                e.mPlayer->sendText("入服通知设置失败!");
-            }
-
-            return false;
-        } else if (e.mMessage.starts_with("-jm:get")) {
-            e.mPlayer->sendText(lymoProjects__::joinMessage::ref().get(e.mPlayer->getXuid()));
-
-            return false;
-        } else if (e.mMessage.starts_with("-jm:erase")) {
-            lymoProjects__::joinMessage::ref().erase(e.mPlayer->getXuid());
-
-            e.mPlayer->sendText("入服通知清除成功!");
-
-            return false;
-        }
-
-        return true;
-    });
+    Event::PlayerJoinEvent::subscribe(lymoProjects__::handlePlayerJoinEvent);
+    Event::PlayerChatEvent::subscribe(lymoProjects__::handlePlayerChatEvent);
 }
